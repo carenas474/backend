672de62ee4ec024407a130618791781bffb0c72d
carenas474 <carenas474@unab.edu.co>
2025-03-25 00:28:22 -0500

    template blade

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="unab-footer text-center">
+    <div class="container">
+        <p class="mb-0">© 2023 Universidad Autónoma de Bucaramanga - Tienda Oficial</p>
+        <div class="mt-2">
+            <a href="#" class="text-white mx-2">Políticas de privacidad</a>
+            <a href="#" class="text-white mx-2">Términos de uso</a>
+            <a href="#" class="text-white mx-2">Soporte técnico</a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,25 @@
+<!-- Navbar -->
+<nav class="unab-header navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand text-white fw-bold" href="#">UNAB Store</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Crear Producto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Administración</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Salir</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
